e1c89b5c787d9ad881bfa30f191a90d2c9fa1b45
nangebav <nangebav@gmail.com>
2021-01-18 22:43:45 -0500

    html y sccript

--- src/cipher.js
+++ src/cipher.js
@@ -2,4 +2,71 @@ const cipher = {
   // ...
 };
 
+//Evento para cifrar
+
+botonCifrar.addEventListener("click",()=>{
+  let numero = inputDesplazar.value;
+  let textoIngresado = inputIngresar.value;
+  let textoCifrado = " ";
+
+    //Identifico el N° ascii del texto ingresaado
+    //0 indica la posición inicial del string, aquí aplicar for
+  for (let i = 0; i < textoIngresado.length; i++) {
+    //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
+  let numeroAscii= textoIngresado.charCodeAt(i);
+  
+  if(numeroAscii >= 65 && numeroAscii <= 90) {
+    //Aplico la fórmula de Cifrado César para el desplazamiento
+    let cifradoMayuscula = (numeroAscii-65+parseInt(numero))%26+65;
+     textoCifrado += String.fromCharCode(cifradoMayuscula);
+     inputResultado.value = textoCifrado;
+    }   
+    
+      else if (numeroAscii >= 97 && numeroAscii <= 122) {
+      let cifradoMinuscula = (numeroAscii-97+parseInt(numero))%26+97;
+      textoCifrado += String.fromCharCode(cifradoMinuscula);
+      inputResultado.value = textoCifrado;
+      
+    }
+
+      else if (numeroAscii <= 32 ) {
+      let espacio = (numeroAscii-32+parseInt)%26+32;
+      textoCifrado += String.fromCharCode(espacio);
+      inputResultado.value = textoCifrado;
+      
+    }
+    
+  } 
+
+});
+
+botonDescifrar.addEventListener("click",()=>{
+  let numero = inputDesplazar.value;
+  let textoIngresado = inputIngresar.value;
+  let textoCifrado = " ";
+
+  for (let i = 0; i < textoIngresado.length; i++) {
+  let numeroAscii= textoIngresado.charCodeAt(i);
+  
+  if(numeroAscii >= 65 && numeroAscii <= 90) {
+    //Aplico la fórmula de Cifrado César para el desplazamiento
+    let cifradoMayuscula = (numeroAscii-65-parseInt(numero))%26+65;
+     textoCifrado += String.fromCharCode(cifradoMayuscula);
+     inputResultado.value = textoCifrado;
+    }       
+      else if (numeroAscii >= 97 && numeroAscii <= 122) {
+      let cifradoMinuscula = (numeroAscii-97-parseInt(numero))%26+97;
+      textoCifrado += String.fromCharCode(cifradoMinuscula);
+      inputResultado.value = textoCifrado;
+    }
+      else if (numeroAscii <= 32 ) {
+      let espacio = (numeroAscii-32-parseInt)%26+32;
+      textoCifrado += String.fromCharCode(espacio);
+      inputResultado.value = textoCifrado;      
+    }   
+  } 
+});
+
+
+MediaElementAudioSourceNode
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+const iniciar = document.getElementById ("iniciar"); //para ir a la segunda página//
+const paginaUno =document.getElementById ("paginaUno");
+const paginaDos =document.getElementById ("paginaDos");
+
+paginaUno.style.display ="block";
+paginaDos.style.display ="none";
+
+iniciar.addEventListener("click",()=>{
+    
+    paginaUno.style.display = "none" ;
+    paginaDos.style.display = "block";
+    });
+
+
+//Declarando variables para cifrar y descifrar
+const inputIngresar = document.getElementById ("inputIngresar"); 
+const inputResultado = document.getElementById ("inputResultado");
+const inputDesplazar = document.getElementById ("inputDesplazar");
+const botonCifrar = document.getElementById ("botonCifrar");
+const botonDescifrar = document.getElementById ("botonDescifrar");
+//const botonLimpiar = document.getElementById ("botonLimpiar");
+
+
+
+
+    //textoCifrado += inputResultado.String.fromCharCode(0);//
+    //charCodeAt devuelve el número de la posición ASCII//
+
+
+
+
+
 import cipher from './cipher.js';
 
 console.log(cipher);
